0d05ff735f88e18d6bca5813e78c31f2d62addb5
EvgenValentine <evgenijkniznickij@gmail.com>
2020-05-29 17:43:56 +0300

    Second merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] massive = new String[5];
+        massive[0] = "Madam, I'm Adam!";
+        massive[1] = "А муза – раба разума";
+        massive[2] = "Ах, Ира – во повариха!";
+        massive[3] = "блабла";
+        massive[4] = "«Во, голое тело!», - вопи по воле теологов";
+
+
+        for (int k = 0; k < massive.length; k++){
+            String text = massive[k];
+//
+            text = text
+                    .replaceAll("[^A-Za-z0-9_А-Яа-я]","")
+                    .toLowerCase();
+            boolean p = true;
+            for (int i = 0, j = text.length()-1; i < text.length()/2;i++, j--){
+                if(text.charAt(i) != text.charAt(j)){
+                    p = false;
+                }
+            }
+            if (p){
+                System.out.println(text + " true");
+            }else {
+                System.out.println(text + " false");
+            }
+        }
+
     }
 }
